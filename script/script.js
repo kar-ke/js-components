27cada6db9fe78f9c0cfe8f4016ca43c6a971d3d
Karthikeyan Ramesh <karthikramesh444@gmail.com>
2024-08-07 13:51:30 +0530

    Tabs task done

--- script/script.js
+++ script/script.js
@@ -14,8 +14,30 @@ titles.forEach((title) => {
         title.parentElement.classList.toggle('active');
     })
 })
-// accordion end ---------------
 
 
 
+// Tabs
+
+const tabs = document.querySelectorAll('.tab-item');
+const contents = document.querySelectorAll('.content');
+
+tabs.forEach((tab, index) => {
+    tab.addEventListener('click', function () {
+        tabs.forEach((tab) => {
+            if (tab.classList.contains('active')) {
+                tab.classList.remove('active');
+            };
+        });
+        tab.classList.add('active')
+
+        contents.forEach((content) => {
+            if (content.classList.contains('active')) {
+                content.classList.remove('active');
+            };
+        });
+        contents[index].classList.add('active');
+
+    });
+});
 
